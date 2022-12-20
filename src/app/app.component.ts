f02841c8f18c14e7368c979aec7c4dd82016a904
ADDOUCHE Walid <73178026+walidaddouche@users.noreply.github.com>
2022-12-20 16:57:03 +0100

    Pokemon card done with materialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,42 @@
 import {Component, OnInit} from '@angular/core';
+import {POKEMONS} from "./mock-pokemon-list";
+import {Pokemon} from "./pokemon";
 
 @Component({
     selector: 'app-root',
-    template:
-        `
-            <h1>
-                Hello {{title}}
-            </h1>
-
-        `
-
+    templateUrl: "app.component.html"
 })
 
 // selector -> le nom du component
 // template -> le code de la page
 
 export class AppComponent implements OnInit {
+
+    pokemonList: Pokemon[] = POKEMONS;
+
+    pokemonSelected: Pokemon | undefined;
+
+
+
     ngOnInit(): void {
+        console.log(this.pokemonList)
+    }
+
+    selectPokemon(index: string) {
+        this.selectPokemonWithId(+index);
+        if (this.pokemonSelected === undefined) {
+            console.log("Vous avez demandé un pokemon qui n'existe pas ")
+        } else {
+            let name = this.pokemonSelected.name;
+            console.log(`Le pokemon selectioné est ${name}`)
+
+        }
 
     }
 
-    selectPokemon(name: string) {
-        console.log(` Vous avez cliqué sur le pokemon ${name} `)
+    selectPokemonWithId(id: number) {
+
+        this.pokemonSelected = this.pokemonList.find(pokemon => pokemon.id === id);
+
     }
-
-
-    title: string = 'ng-pokemon-app';
-
-
 }
